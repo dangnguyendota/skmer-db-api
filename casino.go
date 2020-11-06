bb6d0b80460e1553cf804356bf808459290f718c
Nguyen Dang Nguyen <dangnguyendota@gmail.com>
2020-11-06 10:06:09 +0700

    add docs, change func name

--- casino.go
+++ casino.go
@@ -3,6 +3,10 @@ package skmerdb
 import "github.com/google/uuid"
 
 type SkmerDB interface {
+	// Lấy SkmerUser theo id của tài khoản.
 	GetUserById(id uuid.UUID) SkmerUser
-	UpdateCoins(id uuid.UUID, oldCoins, newCoins int64) SkmerUser
+	// cập point của tài khoản sau đó trả về tài khoản sau khi cập nhật.
+	// o là số point cũ và n là số point mới.
+	// id là id của tài khoản.
+	UpdatePoint(id uuid.UUID, o, n int64) SkmerUser
 }
